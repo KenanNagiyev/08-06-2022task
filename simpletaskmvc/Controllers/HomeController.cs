274ef6f3b0eae2a6f2da5c7377eb785aa03b0238
KenanNagiyev <788lnmp@code.edu.az>
2022-06-07 23:32:19 +0400

    new file

---
+++ simpletaskmvc/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using simpletaskmvc.DAL;
+using simpletaskmvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simpletaskmvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ simpletaskmvc/Models/Slider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace simpletaskmvc.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string About { get; set; }
+        public string Image { get; set; }
+    }
+}
